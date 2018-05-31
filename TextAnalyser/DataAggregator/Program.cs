e2cbb631e53100bc578c61f8036e09dabece71b9
Aleksandre Sukhitashvili <aleksandresukhitashvili@gmail.com>
2018-05-31 20:47:43 +0400

    Just work in progress

---
+++ TextAnalyser/DataAggregator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDir = Directory.GetCurrentDirectory();
+            string output = Path.Combine(inputDir, "output");
+
+            if (args.Length == 2)
+            {
+                inputDir = args[0];
+                output = args[1];
+            }
+
+            if (!Directory.Exists(output))
+                Directory.CreateDirectory(output);
+
+            //Recursively
+            ScrapFiles(inputDir, output);
+
+
+
+
+
+        }
+
+        private static void ScrapFiles(string inputDir, string output)
+        {
+            foreach (var inputFilePath in inputDir.ToDirectoryInfo().EnumerateFiles())
+            {
+                var outputFilePath = Path.Combine(output, inputFilePath.Name);
+                ActuallyScrapFile(inputFilePath.FullName, outputFilePath);//TODO:can be made generic
+            }
+
+            foreach (var inputDirPath in inputDir.ToDirectoryInfo().EnumerateDirectories())
+            {
+                var outputDirPath = Path.Combine(inputDir, inputDirPath.Name);
+                ScrapFiles(inputDir, outputDirPath);
+            }
+        }
+
+        private static void ActuallyScrapFile(string inputFilePath, string outputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class IOExtensions
+    {
+        public static DirectoryInfo ToDirectoryInfo(this string path)
+        {
+            return new DirectoryInfo(path);
+        }
+    }
+}
---
+++ TextAnalyser/WordTextExtractor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace WordTextExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        /// <summary>
+        /// მეთოდი რომელიც წაიკითხავს დოკუმენტს და დააბრუნებს ტექსტს
+        /// </summary>
+        /// <param name="path">დოკუმენტის მისამართი</param>
+        /// <returns></returns>
+        public string ParseDocument(string path)
+        {
+            var word = new Application();
+            var docs = word.Documents.Open(path);
+            var totaltext = "";
+            for (var i = 0; i < docs.Paragraphs.Count; i++)
+            {
+                totaltext += " \r\n " + docs.Paragraphs[i + 1].Range.Text;
+            }
+            docs.Close();
+            word.Quit();
+            return totaltext;
+        }
+    }
+    
+}
