ddec568a62de16b5639838b2dffb269370673f19
Aleksandre Sukhitashvili <aleksandresukhitashvili@gmail.com>
2018-06-18 18:17:15 +0400

    In progress

--- TextAnalyser/LanguageModelAdapter/GeorgianLanguageModel.cs
+++ TextAnalyser/LanguageModelAdapter/GeorgianLanguageModel.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml;
 using TextMarkovChains;
 //using TextMarkovChain = TextAnalyser.TextMarkovChain;
@@ -8,6 +9,7 @@ namespace LanguageModelAdapter
     public class GeorgianLanguageModel : ChainAdapter<TextMarkovChain>
     {
         protected override string XmlFileName { get; } = "geo_model.xml";
+        protected override Action<int> FileBeingLoadedLogger { get; }
 
         protected override IMarkovChain InitializeChain() => new TextMarkovChain();
     }
